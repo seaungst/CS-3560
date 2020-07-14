94332de1f908403425a528f3cd2691d9b1319245
Selena Aungst <selenabean@Selenas-MacBook-Air.local>
2020-07-14 19:02:00 -0400

    updated css

--- src/orderdemo/LoginController.java
+++ src/orderdemo/LoginController.java
@@ -7,9 +7,6 @@ package orderdemo;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -17,8 +14,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -27,16 +22,7 @@ import javafx.stage.Stage;
  * @author alexv
  */
 public class LoginController implements Initializable {
-    public TextField textField;
-    public Label textLabel;
-    public void button(ActionEvent actionEvent) throws SQLException{
-        textLabel.setText(textField.getText());
-        ConnectionClass connectionClass = new ConnectionClass();
-        Connection connection = connectionClass.getConnection();
-        String sql = "INSERT INTO USER VALUES('"+textField.getText()+"')";
-        Statement statement=connection.createStatement();
-        statement.executeUpdate(sql);
-    }
+
     public void changeWelcome(ActionEvent event) throws IOException {
 	Parent createAccountParent = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
 	Scene createAccountScene = new Scene(createAccountParent);
