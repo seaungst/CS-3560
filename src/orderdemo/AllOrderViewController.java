7a8f455bd17485732d20fd9c5aceb0cfb53ef7b7
Selena Aungst <selenabean@Selenas-MacBook-Air.local>
2020-07-15 23:34:21 -0400

    styling update

--- src/orderdemo/AllOrderViewController.java
+++ src/orderdemo/AllOrderViewController.java
@@ -5,9 +5,16 @@
  */
 package orderdemo;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -19,6 +26,17 @@ public class AllOrderViewController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    public void changeManageAccount(ActionEvent event) throws IOException {
+	Parent createAccountParent = FXMLLoader.load(getClass().getResource("ManageAccount.fxml"));
+	Scene createAccountScene = new Scene(createAccountParent);
+
+	// This line gets the Stage information
+	Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+	stage.setScene(createAccountScene);
+	stage.show();
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- src/orderdemo/MenuController.java
+++ src/orderdemo/MenuController.java
@@ -8,17 +8,27 @@ package orderdemo;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author alexv
- */
 public class MenuController implements Initializable {
+            
+    public void changeAllOrderView(ActionEvent event) throws IOException {
+	Parent createAccountParent = FXMLLoader.load(getClass().getResource("AllOrderView.fxml"));
+	Scene createAccountScene = new Scene(createAccountParent);
 
-    /**
-     * Initializes the controller class.
-     */
+	// This line gets the Stage information
+	Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+	stage.setScene(createAccountScene);
+	stage.show();
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
