a7674124b0c5d5070a4303388ec55bc3fbe69258
sun <alex.vo.1993@gmail.com>
2020-07-21 23:02:36 -0700

    up

--- src/orderdemo/AllOrderViewController.java
+++ src/orderdemo/AllOrderViewController.java
@@ -7,13 +7,18 @@ package orderdemo;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 /**
@@ -26,7 +31,7 @@ public class AllOrderViewController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    
+		
     public void changeManageAccount(ActionEvent event) throws IOException {
 	Parent createAccountParent = FXMLLoader.load(getClass().getResource("ManageAccount.fxml"));
 	Scene createAccountScene = new Scene(createAccountParent);
@@ -47,7 +52,19 @@ public class AllOrderViewController implements Initializable {
     } 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            ConnectionClass connectionClass = new ConnectionClass();
+            Connection connection = connectionClass.getConnection();
+            ResultSet result = connection.createStatement().executeQuery("SELECT * FROM foods");
+            while(result.next()){
+                oblist.add(new ModelTable(result.getString(2),result.getDouble(3)));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        item.setCellValueFactory(new PropertyValueFactory<>("name"));
+        price.setCellValueFactory(new PropertyValueFactory<>("unit_price"));
+        menu.setItems(oblist);
     }    
     
 }
